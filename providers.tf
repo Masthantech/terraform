61b100fb0329433114a024081409718b7699d843
khizar <masthantech619@gmail.com>
2026-04-01 15:57:28 +0530

    provider added

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashocorp/aws"
+        version = "6.38.0"
+      }
+    }
+}
+
+provider "aws" {
+    #configuration options
+  
+}
